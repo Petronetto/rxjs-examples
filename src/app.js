670d73153f496f56bfa49981d06974addba8d1a3
Juliano Petronetto <Juliano Petronetto>
2016-09-29 20:32:54 -0300

    Example 3

--- src/app.js
+++ src/app.js
@@ -1,17 +1,8 @@
 import $ from 'jquery'
 import Rx from 'rxjs/Rx'
+import { getSubscriber } from './utils/getSubscriber'
 
-var nums = [33,45,12,88,15,98,20,23,58]
-var nums$ = Rx.Observable.from(nums)
+const nums = [33,45,12,88,15,98,20,23,58]
+const nums$ = Rx.Observable.from(nums)
 
-nums$.subscribe(
-  x => {
-    console.log(x)
-  },
-  err => {
-    console.log(err)
-  },
-  complete => {
-    console.log('Completed')
-  }
-)
+nums$.subscribe(getSubscriber('nums'))
---
+++ src/utils/getSubscriber.js
@@ -0,0 +1,13 @@
+export function getSubscriber(id) {
+  return {
+    next(x) {
+      console.log(`${id}: ${x}`)
+    },
+    error(err) {
+      console.log(`${id}: ${err}`)
+    },
+    complete(err) {
+      console.log(`${id}: Completed!`)
+    }
+  }
+}

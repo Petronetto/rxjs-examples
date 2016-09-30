0597a4f0d233df8602ebc99a04e7abe92c8436de
Juliano Petronetto <juliano@petronetto.com.br>
2016-09-30 14:46:40 -0300

    Example 10

--- src/app.js
+++ src/app.js
@@ -2,32 +2,19 @@ import $ from 'jquery'
 import Rx from 'rxjs/Rx'
 import { getSubscriber } from './utils/getSubscriber'
 
-// Range
-const range$ = new Rx.Observable.range(0, 10)
-  .subscribe(getSubscriber('Range'))
-
-// Interval
-const interval$ = new Rx.Observable.interval(500)
-  .take(5) // o take limita a quantidade de iterações, se for removido gera um loop infinito
-  .subscribe(getSubscriber('Interval'))
-
-// Timer
-const timer$ = new Rx.Observable.timer(3000, 500)
-  .take(5) // o take limita a quantidade de iterações, se for removido gera um loop infinito
-  .subscribe(getSubscriber('Timer'))
-
-// Of
-const of$ = new Rx.Observable.of(86, 'Hello World', [2,3,4,5,6], {name: 'Jon Doe'})
-  .subscribe(getSubscriber('Of'))
-
-// Defer
-let i = 0
-const defer$ = new Rx.Observable.defer(() => {
-  i++
-  return Rx.Observable.of(i)
+const myPromise = new Promise((resolve, reject) => {
+  console.log('Creating Promise...')
+  setTimeout(() => {
+    console.log('Do something...')
+    resolve('Resolved!')
+  }, 2000)
 })
 
-defer$.subscribe(getSubscriber('Defer 1'))
-defer$.subscribe(getSubscriber('Defer 2'))
-defer$.subscribe(getSubscriber('Defer 3'))
+// Usando Promises
+// myPromise.then(data => {
+//   console.log(data)
+// })
 
+// Usando Observables
+Rx.Observable.fromPromise(myPromise)
+  .subscribe(getSubscriber('Promise'))

1148e57f53ff54b767ecfc51a8dff8169f32528f
Juliano Petronetto <juliano@petronetto.com.br>
2016-09-30 02:38:01 -0300

    Example 9

--- src/app.js
+++ src/app.js
@@ -3,16 +3,31 @@ import Rx from 'rxjs/Rx'
 import { getSubscriber } from './utils/getSubscriber'
 
 // Range
-const sourceOne$ = new Rx.Observable.range(0, 10)
+const range$ = new Rx.Observable.range(0, 10)
   .subscribe(getSubscriber('Range'))
 
 // Interval
-const sourceTwo$ = new Rx.Observable.interval(500)
+const interval$ = new Rx.Observable.interval(500)
   .take(5) // o take limita a quantidade de iterações, se for removido gera um loop infinito
   .subscribe(getSubscriber('Interval'))
 
 // Timer
-const sourceThree$ = new Rx.Observable.timer(3000, 500)
+const timer$ = new Rx.Observable.timer(3000, 500)
   .take(5) // o take limita a quantidade de iterações, se for removido gera um loop infinito
   .subscribe(getSubscriber('Timer'))
+
+// Of
+const of$ = new Rx.Observable.of(86, 'Hello World', [2,3,4,5,6], {name: 'Jon Doe'})
+  .subscribe(getSubscriber('Of'))
+
+// Defer
+let i = 0
+const defer$ = new Rx.Observable.defer(() => {
+  i++
+  return Rx.Observable.of(i)
+})
+
+defer$.subscribe(getSubscriber('Defer 1'))
+defer$.subscribe(getSubscriber('Defer 2'))
+defer$.subscribe(getSubscriber('Defer 3'))
 

d6f8c4b412bc7ed7f96d2efec664c532eb93ef0b
Juliano Petronetto <Juliano Petronetto>
2016-09-29 21:11:02 -0300

    Example 5

--- src/app.js
+++ src/app.js
@@ -2,12 +2,26 @@ import $ from 'jquery'
 import Rx from 'rxjs/Rx'
 import { getSubscriber } from './utils/getSubscriber'
 
-const users = [
-  {name: 'Jon Snow', email: 'youknownothing@mail.com'},
-  {name: 'Daenerys Targaryen', email: 'khaleesi@gmail.com'},
-  {name: 'Tyrion Lannister', email: 'halfman@gmail.com'}
-]
+// Stream de um Set
+console.log('Stream de um Set')
+const s = new Set(['Foo', 43, {name: 'John Doe'}])
 
-const users$ = Rx.Observable.from(users)
+const s$ = Rx.Observable.from(s)
 
-users$.subscribe(getSubscriber('users'))
+s$.subscribe(getSubscriber('set'))
+
+// Stream de um Map
+console.log('Stream de um Map')
+const m = new Map([[1,2], [3,4], [5,6]])
+
+const m$ = Rx.Observable.from(m)
+
+m$.subscribe(getSubscriber('map'))
+
+// Stream de uma String
+console.log('Stream de uma String')
+const str = 'Hello World!'
+
+const str$ = Rx.Observable.from(str)
+
+str$.subscribe(getSubscriber('string'))
--- src/utils/getSubscriber.js
+++ src/utils/getSubscriber.js
@@ -1,7 +1,7 @@
 export function getSubscriber(id) {
   return {
     next(x) {
-      console.log(`${id}-> Name: ${x.name} E-mail: ${x.email}`)
+      console.log(`${id}-> ${x}`)
     },
     error(err) {
       console.log(`${id}-> ${err}`)

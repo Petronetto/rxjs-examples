3cea79d912d00aa7133786870ce56f63240e90e5
Juliano Petronetto <Juliano Petronetto>
2016-09-30 00:38:23 -0300

    Example 9

--- src/app.js
+++ src/app.js
@@ -2,6 +2,12 @@ import $ from 'jquery'
 import Rx from 'rxjs/Rx'
 import { getSubscriber } from './utils/getSubscriber'
 
-const source$ = new Rx.Observable.interval(500)
+// Interval
+const sourceOne$ = new Rx.Observable.interval(500)
   .take(5) // o take limita a quantidade de iterações, se for removido gera um loop infinito
   .subscribe(getSubscriber('Interval'))
+
+// Timer
+const sourceTwo$ = new Rx.Observable.timer(3000, 500)
+  .take(5) // o take limita a quantidade de iterações, se for removido gera um loop infinito
+  .subscribe(getSubscriber('Timer'))
